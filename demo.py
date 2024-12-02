dbf12796fbcf9ca6605576d82e49e81d7030a330
lkpk1994 <38982677+lkpk1994@users.noreply.github.com>
2024-12-02 15:10:08 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -14,5 +14,8 @@ async def shop():
     return {"shop": "商品信息"}
 
 
+print('ccccccccccccccccccccc')
+
+
 if __name__ == '__main__':
     uvicorn.run("demo:app", host='127.0.0.1', port=8080, reload=True)
